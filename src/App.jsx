c9426f4a3c74dbccf2d41b309b80df40155bb1fe
tepawar <tejaspavara@gmail.com>
2023-11-19 21:23:43 +0530

    install reactjs and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-red-300 flex items-center  justify-center h-screen '>App</div>
+    </>
+  )
+}
+
+export default App
